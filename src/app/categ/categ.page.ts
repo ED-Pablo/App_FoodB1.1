1a8e9e4a887809816af6ad2c32774b4cf4a89092
ED-Pablo <espinozapablouets@gmail.com>
2023-11-23 21:43:21 -0500

    App_FoodB1.1

---
+++ src/app/categ/categ.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categ',
+  templateUrl: './categ.page.html',
+  styleUrls: ['./categ.page.scss'],
+})
+export class CategPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
